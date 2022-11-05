5ac7832c6eae2d500baf7107dc48bcf7d76bb083
Ahmed-Moudud <suzan.4bd@gmail.com>
2022-11-05 15:09:54 +0600

    completed

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,41 @@
+import Mian from "../../Layout/Mian";
+import CheckOut from "../../Pages/CheckOut/CheckOut";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Orders from "../../Pages/Orders/Orders";
+import SignUp from "../../Pages/SignUp/SignUp";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Mian></Mian>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        },
+        {
+          path: '/signup',
+          element: <SignUp></SignUp>
+        },
+        {
+          path: '/checkout/:id',
+          element: <PrivateRoute><CheckOut></CheckOut></PrivateRoute>,
+          loader: ({params})=> fetch(`https://genius-car-server-ivory.vercel.app/services/${params.id}`)
+        },
+        {
+          path: '/orders',
+          element: <PrivateRoute><Orders></Orders></PrivateRoute>
+        }
+      ]
+    }
+  ]);
+
+  export default router;
